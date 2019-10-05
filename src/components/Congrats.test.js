bd0f878eddf2f02b0eb305817958d058ac0cad7c
Gabriel Aniora <chybesta@gmail.com>
2019-10-05 19:29:22 +0100

    made congrats component assertions

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -2,6 +2,37 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttribute } from '../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * function to create a ShallowWrapper for the Congrats component.
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}) => {
+  props = { success: true };
+  return shallow(<Congrats {...props} />);
+} 
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttribute(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
+});
+
+test('renders no text when `success` prop is false', () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttribute(wrapper, 'component-congrats');
+  expect(component.text()).toBe("");
+});
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttribute(wrapper, 'congrats-message');
+  expect(message.text().length).not.toBe(0);
+});
+
+ 
