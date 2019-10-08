29ee9c38952625b001bf8c68f74a03ace0953a72
Gabriel Aniora <chybesta@gmail.com>
2019-10-08 17:11:21 +0100

    set up Input component and storeFactory test util

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,4 +1,18 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../../src/redux/reducers/rootReducer';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state. 
+ * globals: rootReducer.
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -12,10 +26,10 @@ export const findByTestAttribute = (wrapper, val) => {
 /**
  * checks component prop(s) 
  * @param {jsx} component - React component
- * @param { object } conformingProps - props the component receives
+ * @param {object} conformingProps - props the component receives
  */
-
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
   expect(propError).toBeUndefined();
 }
+
