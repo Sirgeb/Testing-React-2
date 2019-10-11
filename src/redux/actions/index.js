c8fb3dc0857a330f01bd04d17b8ebc126422f511
Gabriel Aniora <chybesta@gmail.com>
2019-10-11 09:53:04 +0100

    add thunk for guessed word piece of state

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from '../../helpers/index';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
@@ -9,8 +11,18 @@ export const actionTypes = {
  * @param {string} guessWord - Guessed word,
  * @returns {function} - Redux Thunk function.
  */
-export const guessWord = (guessWord) => {
+export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
-    
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
   };
 };
